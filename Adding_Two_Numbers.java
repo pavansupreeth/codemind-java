3f36e50a3d7243649847680f4816adcec5796462
pavansupreeth <147490564+pavansupreeth@users.noreply.github.com>
2024-07-31 15:07:50 +0530

    import java.util.Scanner; public class demo{     public static void main(String[] args){         Scanner scanner=new Scanner(System.in);         int a=scanner.nextInt();         int b=scanner.nextInt();         int c =a+b;         System.out.println(c);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class demo{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c =a+b;
+        System.out.println(c);
+    }
+}
